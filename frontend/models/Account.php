0d33467d595d32f5eda90b9cac39d1d0e6726c3b
Алексей Чаплыгин <a.chaplygin@freematiq.com>
2019-03-14 13:04:11 +0700

    models\Account

---
+++ frontend/models/Account.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "account".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $balance
+ *
+ * @property User $user
+ * @property Transaction[] $transactions
+ * @property Transaction[] $transactions0
+ */
+class Account extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'account';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id'], 'required'],
+            [['user_id'], 'default', 'value' => null],
+            [['user_id'], 'integer'],
+            [['balance'], 'number'],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'user_id' => 'User ID',
+            'balance' => 'Balance',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTransactions()
+    {
+        return $this->hasMany(Transaction::className(), ['account_user' => 'id']);
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getTransactions0()
+    {
+        return $this->hasMany(Transaction::className(), ['account_system' => 'id']);
+    }
+}
